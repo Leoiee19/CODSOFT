4dc7c9264ec79dc59add753b5a55a0168ab4604b
Leoiee19 <lohithaswinonline2005@gmail.com>
2024-07-06 00:07:53 +0530

    Update Number Guessing Game.cpp

--- Number Guessing Game.cpp
+++ Number Guessing Game.cpp
@@ -1,37 +1,30 @@
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <iostream>
 using namespace std;
-
 int main()
 {
-
-    cout << "*******Welcome to GuessTheNumber game********"
-         << endl;
-    cout << "Guess the Number between 1 and 100 to win this game "<< endl;
-srand(time(0));
-int won=0;
-    while (true) {
-      
-        int random = 1 + (rand() % 100);
-        int guess;
-        while(!won){
+    cout << "*******Welcome to GuessTheNumber game********" << endl;
+     int won = 0;
+    srand(time(0));
+    int random = 1 + (rand() % 100);
+    int guess;
+    cout << "Guess the number to win" << endl;
+    do {
         cout << "\nEnter the number: ";
         cin >> guess;
-        
-    if (guess == random) {
-    cout << "Congrats! You won, "<< guess << " is the Correct number" << endl;
-    cout << "\t\t\t Thanks for playing...."<< endl;
-    won=1;
-  break;}
-    else {
-           cout << "Wrong " << endl;
-                    if (guess > random) {
-                     cout << "The Correct number is smaller than the number" << endl;}
-                    else {
-                        cout << "The Correct number is greater than the number "
-                             << endl;
-                    }}}}
-                   
+        if (guess == random) {
+            cout << "ggs,You won " << endl;
+            won = 1;
+        } else {
+            cout << "Wrong " << endl;
+            if (guess > random) {
+                cout << "The number is smaller than the guess" << endl;
+            } else {
+                cout << "The number is greater than the guess" << endl;
+            }
+        }
+    } while (!won);
+
     return 0;
 }
