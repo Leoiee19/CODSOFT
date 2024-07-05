f2cd6a40ee3612683b239faea9ca55e7d33d6d20
Leoiee19 <lohithaswinonline2005@gmail.com>
2024-07-06 00:08:52 +0530

    Update Simple calculator.cpp

--- Simple calculator.cpp
+++ Simple calculator.cpp
@@ -1,38 +1,33 @@
 
 #include <iostream>
 using namespace std;
-
 int main(){
-    
-float n1, n2;
-char operation;
+    float n1, n2;
+char k;
 cout<<"****************SIMPLE CALCULATOR*************"<<endl;
 
 cout<<"Enter First number: ";
 cin >>n1;
 cout<<"Enter Second number: ";
 cin>> n2;
-cout<< "ENTER A ARITHMETIC OPERATION TO PERFORM: ";
-cin >>operation;
+cout<< "ENTER A ARITHMETIC OPERATION TO PERFORM:\n + FOR ADDITION\n- FOR SUBRACTION\n* FOR MULTIPLICATION\n/ FOR DIVISION ";
+cin >>k;
 
-switch (operation) {
-case '+':
+switch (k) { 
+	case '+':
 	cout << "The Result is "<<n1+n2;
 	break;
-
 case '-':
 	cout <<"The Result is "<< n1-n2;
     break;
 case '*':
 	cout <<"The Result is "<< n1*n2;
 	break;
-
 case '/':
 	cout <<"The Result is "<< n1/n2;
 	break;
 
 default:
-	cout << "Error";
-	}
+	cout << "invalid";}
 	return 0;
 }
